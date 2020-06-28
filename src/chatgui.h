a282cfdfc2357f77bd661546537d0dd863511ecc
Michael Harris <michael.harris@block.one>
2020-06-27 21:57:52 -0400

    Try .get method

--- src/chatgui.h
+++ src/chatgui.h
@@ -29,7 +29,7 @@ public:
 
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
+    ChatLogic &GetChatLogicHandle() const { return *_chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
